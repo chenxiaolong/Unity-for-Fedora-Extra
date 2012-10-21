f8d64c409e8fbd08f4e7023fb021d055cf927def
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-20 21:27:29 -0400

    [remote-login-service] Version comparison script: Parse Launchpad webpage to get Ubuntu version

--- remote-login-service/compare_versions.sh
+++ remote-login-service/compare_versions.sh
@@ -3,7 +3,7 @@
 SPEC_VER="$(rpmspec -q --qf '%{version}\n' remote-login-service.spec | head -1)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/remote-login-service' -O - | sed -n 's/.*>remote-login-service_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/remote-login-service' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/remote-login-service/+download' -O - | sed -n 's/.*remote-login-service-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
