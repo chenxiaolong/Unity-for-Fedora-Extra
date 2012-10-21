2e32b238244d5b159e0cce022f4924d2dc073076
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-20 21:24:41 -0400

    [thin-client-config-agent] Version comparison script: Parse Launchpad webpage to get Ubuntu version

--- thin-client-config-agent/compare_versions.sh
+++ thin-client-config-agent/compare_versions.sh
@@ -3,7 +3,7 @@
 SPEC_VER="$(rpmspec -q --qf '%{version}\n' thin-client-config-agent.spec | head -1)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/thin-client-config-agent' -O - | sed -n 's/.*>thin-client-config-agent_\(.*\)\.tar\.gz<.*/\1/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/thin-client-config-agent' | sed -n 's/^.*current\ release\ (\(.*\)).*$/\1/p'))
 
 echo ""
 
