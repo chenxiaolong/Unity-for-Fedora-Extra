6f811e7b5f045edc56e531e588438af13c9c8cd6
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-20 20:47:36 -0400

    [ubuntu-sounds] Version comparison script: Parse Launchpad webpage to get Ubuntu version

--- ubuntu-sounds/compare_versions.sh
+++ ubuntu-sounds/compare_versions.sh
@@ -3,7 +3,7 @@
 SPEC_VER="$(rpmspec -q --qf '%{version}\n' ubuntu-sounds.spec | head -1)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/ubuntu-sounds' -O - | sed -n 's/.*>ubuntu-sounds_\(.*\)\.tar\.gz<.*/\1/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/ubuntu-sounds' | sed -n 's/^.*current\ release\ (\(.*\)).*$/\1/p'))
 
 echo ""
 
