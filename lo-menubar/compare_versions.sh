4f524a7382bbdbd04915c277623cd889a1602922
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-20 21:15:56 -0400

    [lo-menubar] Version comparison script: Parse Launchpad webpage to get Ubuntu version

--- lo-menubar/compare_versions.sh
+++ lo-menubar/compare_versions.sh
@@ -3,7 +3,7 @@
 SPEC_VER="$(rpmspec -q --qf '%{version}\n' lo-menubar.spec | head -1)"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/lo-menubar' -O - | sed -n 's/.*>lo-menubar_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/lo-menubar' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/lo-menubar/+download' -O - | sed -n 's/.*lo-menubar-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
