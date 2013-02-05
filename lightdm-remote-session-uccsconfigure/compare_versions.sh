6dbb2fc0bafdc24dce24008c7d2f136123373ca8
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-02-05 16:41:30 -0500

    [lightdm-remote-session-uccsconfigure] Use new version comparison script

--- lightdm-remote-session-uccsconfigure/compare_versions.sh
+++ lightdm-remote-session-uccsconfigure/compare_versions.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-SPEC_VER="$(rpmspec -q --qf '%{version}\n' lightdm-remote-session-uccsconfigure.spec | head -1)"
+SPECFILE=lightdm-remote-session-uccsconfigure.spec
 
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/lightdm-remote-session-uccsconfigure' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+source "$(dirname ${0})/../version_checker.sh"
 
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/lightdm-remote-session-uccsconfigure/+download' -O - | sed -n 's/.*lightdm-remote-session-uccsconfigure-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
-
-echo ""
-
-echo -e "spec file version: ${SPEC_VER}"
-echo -e "Upstream version:  ${UPSTREAM_VER}"
-echo -e "Ubuntu version:    ${UBUNTU_VER[@]}"
+echo -e "spec file version: $(get_spec_version)"
+echo -e "Upstream version:  $(get_launchpad_version lightdm-remote-session-uccsconfigure)"
+echo -e "Ubuntu version:    $(get_ubuntu_version lightdm-remote-session-uccsconfigure ${1:-raring})"
