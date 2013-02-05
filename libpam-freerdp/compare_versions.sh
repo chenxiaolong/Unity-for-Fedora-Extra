ffa0358a8699a44aba024c9f0f529909b8942888
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-02-05 16:50:41 -0500

    [libpam-freerdp] Use new version comparison script

--- libpam-freerdp/compare_versions.sh
+++ libpam-freerdp/compare_versions.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-SPEC_VER="$(rpmspec -q --qf '%{version}\n' libpam-freerdp.spec | head -1)"
+SPECFILE=libpam-freerdp.spec
 
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/libpam-freerdp' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+source "$(dirname ${0})/../version_checker.sh"
 
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/libpam-freerdp/+download' -O - | sed -n 's/.*libpam-freerdp-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
-
-echo ""
-
-echo -e "spec file version: ${SPEC_VER}"
-echo -e "Upstream version:  ${UPSTREAM_VER}"
-echo -e "Ubuntu version:    ${UBUNTU_VER[@]}"
+echo -e "spec file version: $(get_spec_version)"
+echo -e "Upstream version:  $(get_launchpad_version libpam-freerdp)"
+echo -e "Ubuntu version:    $(get_ubuntu_version libpam-freerdp ${1:-raring})"
